f9dd2c8262a1b9938b51fc85eae26c7243631df2
uf-code <ugur.firat.developer@gmail.com>
2022-06-20 00:06:46 +0300

    implementing bootstrap into next js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Table } from 'react-bootstrap'
+
+import TableComponent from '../components/table.component.jsx'
 
 export default function Home() {
     return (
@@ -12,39 +13,8 @@ export default function Home() {
                 <link rel='icon' href='/favicon.ico' />
             </Head>
 
-            <main>
-                <div>
-                    <Table striped bordered hover variant='dark'>
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                                <th>Username</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td colSpan={2}>Larry Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </div>
-            </main>
+            <main></main>
+            <TableComponent />
         </div>
     )
 }
