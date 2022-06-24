4a92d38073ee90ac47faae90184c3d7c0395f0cd
uf-code <ugur.firat.developer@gmail.com>
2022-06-25 01:25:37 +0300

    table component

--- frontend/components/table.component.jsx
+++ frontend/components/table.component.jsx
@@ -10,41 +10,43 @@ const TableComponent = () => {
     const dispatch = useDispatch()
 
     return (
-        <Table striped bordered hover variant='dark'>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-                {customers.map((customer) => (
+        <div>
+            <Table striped bordered hover variant='dark'>
+                <thead>
                     <tr>
-                        <td> {customer.id}</td>
-                        <td> {customer.first_name} </td>
-                        <td> {customer.last_name} </td>
-                        <td> {customer.email} </td>
-                        <td>
-                            <Button variant='warning'>Edit</Button>
-                        </td>
-                        <td>
-                            <Button
-                                variant='danger'
-                                onClick={() => {
-                                    dispatch(deleteCustomer(customer.id))
-                                }}
-                            >
-                                Delete
-                            </Button>
-                        </td>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
                     </tr>
-                ))}
-            </tbody>
-        </Table>
+                </thead>
+                <tbody>
+                    {customers.map((customer) => (
+                        <tr>
+                            <td> {customer.id}</td>
+                            <td> {customer.first_name} </td>
+                            <td> {customer.last_name} </td>
+                            <td> {customer.email} </td>
+                            <td>
+                                <Button variant='warning'>Edit</Button>
+                            </td>
+                            <td>
+                                <Button
+                                    variant='danger'
+                                    onClick={() => {
+                                        dispatch(deleteCustomer(customer.id))
+                                    }}
+                                >
+                                    Delete
+                                </Button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </div>
     )
 }
 
